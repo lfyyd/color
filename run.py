94d9ba1cf411571806bb06a06692e6c1b097de07
lf <2895074613@qq.com>
2022-07-01 09:17:08 +0800

    删除allure报告路径

--- run.py
+++ run.py
@@ -1,12 +1,12 @@
 import pytest
 import os
 
-if __name__ == '__main__':
-    pytest.main()
-
 # if __name__ == '__main__':
-#     pytest.main(['--alluredir','./color/test_file/allure_results'])
-#     os.system('allure generate ./color/test_file/allure_results  -o ./color/test_file/reports' )
+#     pytest.main()
+
+if __name__ == '__main__':
+    pytest.main(['--alluredir','./color/test_file/allure_results'])
+    os.system('allure generate ./color/test_file/allure_results  -o ./color/test_file/reports' )
 
 # 生成html报告，指定目录在前面加路径
 # if __name__ == '__main__':
